718aa2d3af0e548407a180e2f7ef919405a09874
HectorGit <hectorandres.pv@gmail.com>
2015-03-13 09:25:47 -0700

    Create Help.java

---
+++ Help.java
@@ -0,0 +1,15 @@
+package com.androiddev.hectorperez.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Help extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+
+}
